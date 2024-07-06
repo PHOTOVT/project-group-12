ec38cbad19ef185fe1fe18cfb38832bc33e2b5b9
IrynaKryvenko <kryvenkoirina93@gmail.com>
2024-07-06 12:37:40 +0300

    FAQ/ CSS&JS

---
+++ src/js/faq.js
@@ -0,0 +1,17 @@
+// Встановлено Accordion
+import faqAccordion from 'accordion-js';
+import 'accordion-js/dist/accordion.min.css';
+
+const accordionFaq = new faqAccordion('.accordion-container-faq', {
+    duration: 400,
+    showMultiple: true,
+});
+
+const faqButtons = document.querySelectorAll('.faq-show-hidden-text-button');
+
+faqButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const arrowIcon = button.querySelector('.faq-button-icon');
+        arrowIcon.classList.toggle('arrow-up');
+    })
+});
