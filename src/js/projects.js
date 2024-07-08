eeedbe6ceceaba2963f88f32b4566ef172576d76
VovanOdessa1991 <50461900+VovanOdessa1991@users.noreply.github.com>
2024-07-09 00:34:02 +0200

    slider js

--- src/js/projects.js
+++ src/js/projects.js
@@ -1,16 +1,41 @@
 export const projectRefs = {
   btnLeft: document.querySelector('.project__button-left'),
   btnRight: document.querySelector('.project__button-right'),
-  //   loader: document.querySelector('.loader'),
-  //   preLoader: document.querySelector('.preLoader'),
-  //   nextImg: document.querySelector('.nextImg'),
+  slider: document.querySelectorAll('.project__item'),
 };
+
+let projectIndex = 0;
+
 projectRefs.btnLeft.addEventListener('click', clickLeft);
 projectRefs.btnRight.addEventListener('click', clickRight);
 
+projectRefs.slider[projectIndex].classList.add('active');
+isActiveBtn();
+
 function clickLeft() {
-  console.log('Left!');
+  projectRefs.slider[projectIndex].classList.remove('active');
+
+  --projectIndex;
+  projectRefs.slider[projectIndex].classList.add('active');
+  isActiveBtn();
 }
 function clickRight() {
-  console.log('Right!');
+  projectRefs.slider[projectIndex].classList.remove('active');
+  ++projectIndex;
+  projectRefs.slider[projectIndex].classList.add('active');
+
+  isActiveBtn();
+}
+console.log('projectRefs.slider');
+console.log(projectRefs.slider.length);
+
+function isActiveBtn() {
+  const projectLength = projectRefs.slider.length;
+  if (projectIndex <= 0) {
+    projectRefs.btnLeft.setAttribute('disabled', '');
+  } else projectRefs.btnLeft.removeAttribute('disabled');
+
+  if (projectIndex >= projectLength - 1) {
+    projectRefs.btnRight.setAttribute('disabled', '');
+  } else projectRefs.btnRight.removeAttribute('disabled');
 }
